7e2347109d4d89997222ba8b00b6de1ebf65352b
eodabasioglu <elifodabasi14@gmail.com>
2021-09-03 22:01:17 +0300

    reportstatus entitisi için servis yazıldı.

---
+++ Business/ReportStatusOps/ReportStatusService.cs
@@ -0,0 +1,30 @@
+﻿using DataAccess.EntityFramework.ReportStatusDal;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ReportStatusOps
+{
+    public class ReportStatusService : IReportStatusService
+    {
+        IReportStatusDal _reportStatusDal;
+
+        public ReportStatusService(IReportStatusDal reportStatusDal)
+        {
+            _reportStatusDal = reportStatusDal;
+        }
+
+        public ReportStatus Find(int id)
+        {
+            return _reportStatusDal.Find(x => x.Id == id);
+        }
+
+        public List<ReportStatus> GetAll()
+        {
+            return _reportStatusDal.GetAll().ToList();
+        }
+    }
+}

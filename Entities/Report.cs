547fb1e060b7c3c9003eea776e8ba53d039044f8
eodabasioglu <elifodabasi14@gmail.com>
2021-09-03 19:41:05 +0300

    Katmanlara kullanılacak olan gerekli paketler yüklendi, rapor servis için modeller yazıldı.

---
+++ Entities/Report.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+   public class Report : IEntity
+    {
+        public Report()
+        {
+            ReportStatuses = new Collection<ReportStatus>();
+        }
+
+        public Guid Id { get; set; }
+        public DateTime ReportDemandDate { get; set; }
+        public int ReportStatusId { get; set; }
+        public ICollection<ReportStatus> ReportStatuses { get; set; }
+    }
+}
---
+++ Entities/ReportStatus.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class ReportStatus : IEntity
+    {
+        public ReportStatus()
+        {
+
+        }
+
+        public int Id { get; set; }
+        public string Status { get; set; }
+
+    }
+}
